b2b7c8d16289c9c8323542d78840cb15f2f7e0a6
danny <daniel_coleman@student.uml.edu>
2024-02-04 23:08:59 -0500

    n/a

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include "header.h"
 
+int main(int argumentCount, char **argumentValues) {
+    
+    if (argumentCount != 2) return 1;
+ 
+    FILE *userInputFile;
+    userInputFile = fopen(argumentValues[1], "r");; // "r" stands for "read file" and open file
+
+    int number;
+    int queue[100], stack[100], list[100]; // Queue, stack, and linked list
+    int queuefront = 0
+    int stackpointer = 0
+    int listlength = 0;
+
+    while (fscanf(userInputFile, "%d", &number) == 1) {
+        
+        queue[queuefront++] = number;
+        stack[stackpointer++] = number;
+        insertIntoList(list, &listlength, number); // Insert into simulated queue, stack, and linked list
+    }
+
+    
+    fclose(userInputFile); // Close input file
+
+    
+    printf("QUEUE CONTENTS: "); // Outputs the numbers of queue contents
+    printContents(queue, queuefront);
+
+    printf("STACK CONTENTS: "); // Outputs the numbers of stack contents
+    printContents(stack, stackpointer);
+
+    printf("LIST CONTENTS: "); // Outputs the numbers of list contents
+    printContents(list, listlength);
+
+    return 0;
+}
